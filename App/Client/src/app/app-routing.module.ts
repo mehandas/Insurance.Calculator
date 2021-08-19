72f256193ed2b2fd3c63eeee0391d196f21554a3
Binu Dhas <mehandas@gmail.com>
2021-08-19 22:54:32 +1000

    feat: navigate to applicant-detail component on load

--- App/Client/src/app/app-routing.module.ts
+++ App/Client/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicantDetailComponent } from './applicant-detail/applicant-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'applicant-detail', component: ApplicantDetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
