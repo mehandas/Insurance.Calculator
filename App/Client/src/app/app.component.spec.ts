b96f7939dfbfd2921f79aa2305e81eeb535e3deb
Binu Dhas <mehandas@gmail.com>
2021-08-19 23:06:30 +1000

    test: test spec for app component changes

--- App/Client/src/app/app.component.spec.ts
+++ App/Client/src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { componentFactoryName } from '@angular/compiler';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,16 +19,30 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;    
+  });
+
+  it('should create the app component', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.title')?.textContent).toContain('Insurance calculator');
   });
+
+  it('should navigate to applicant-detail on page load', inject([Router], (router: Router) =>{
+    // Arrange
+    spyOn(router, 'navigate');
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    expect(router.navigate).toHaveBeenCalledWith(['applicant-detail']);
+  }));
 });
--- App/Client/src/app/app.component.ts
+++ App/Client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public router: Router) {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
