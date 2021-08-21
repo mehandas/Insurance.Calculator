c0cb3dc6c08c317c2eded91f878991b7a5c3ad08
Binu Dhas <mehandas@gmail.com>
2021-08-21 21:34:04 +1000

    feat: new service to get invoke refData api

---
+++ App/Client/src/app/common/common.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CommonService } from './common.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { of, throwError } from 'rxjs';
+
+describe('CommonService', () => {
+  let service: CommonService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(CommonService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Method: getRefData', () => {
+    it('should return reference data', inject([HttpClient], (http: HttpClient) => {
+      // Arrange
+      let response: any;
+      const refData = { occupations: [{ occupation: { id: 1, name: 'test' } }] };
+      spyOn(http, 'get').and.returnValue(of(refData));
+
+      // Act
+      service.getRefData().subscribe(res => { response = res; });
+
+      // Assert
+      expect(response).toBe(refData);
+    }));
+
+    it('should return reference data', inject([HttpClient], (http: HttpClient) => {
+      // Arrange
+      let response: any;
+      const errorResponse = new HttpErrorResponse({ error: 'Server error', status: 500 });
+      spyOn(http, 'get').and.returnValue(throwError(errorResponse));
+
+      // Act
+      service.getRefData().subscribe(() => fail(), (error: HttpErrorResponse) => { response = error; });
+
+      // Assert
+      expect(response).toEqual(errorResponse);
+    }));
+  });
+});
---
+++ App/Client/src/app/common/common.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment as env  } from '../../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getRefData(): Observable<any> {
+    const url = `${env.baseUrl}/refData`;
+    return this.httpClient.get<any>(url)
+      .pipe(
+        map(response => response,
+        catchError(err => throwError(err)))
+      );
+  }
+}
